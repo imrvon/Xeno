7f907c6b5f88f1fcfcf38717b5140158861fc665
VoN <hi@imrvon.com>
2022-10-15 14:48:35 +0100

    Settings Fixed

--- xenocoders/xenocoders/settings.py
+++ xenocoders/xenocoders/settings.py
@@ -136,10 +136,10 @@ EMAIL_PORT = 26
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
